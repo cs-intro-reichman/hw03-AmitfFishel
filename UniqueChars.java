bc79a4ecef1545042c5dc398e6e2647738c78fc8
AmitfFishel <154337319+AmitfFishel@users.noreply.github.com>
2024-01-08 14:45:10 +0200

    SOLVED

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,25 +11,18 @@ public class UniqueChars {
           for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i); 
 
-            if (isSolution.indexOf(currentChar) == -1 ||currentChar == ' ') {
+            if (isSolution.indexOf(currentChar) == -1 ||(currentChar == ' ')) {
               isSolution += currentChar;
 
-              for (int j = i + 1; j < str.length()-1; j++) {
-            char nextChar = str.charAt(j); 
-           // char noChar = ' '; 
-
-             if (currentChar != nextChar ) {
-              if (isSolution.indexOf(nextChar) == -1 || nextChar == ' ' )
-              isSolution += nextChar;
-
-                  
+              
+              
               }
 
-          }
+          
         
        }
 
-    }
+    
 
                return isSolution;
 
